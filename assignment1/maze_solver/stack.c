44dd7a2983022ce22cb5566231232b355ae7dad8
Kraiwin-Namsri <kraiwinrubian@gmail.com>
2023-11-01 14:41:25 +0100

    Implemented stack

--- assignment1/maze_solver/stack.c
+++ assignment1/maze_solver/stack.c
@@ -1,40 +1,75 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "stack.h"
 
 struct stack {
-    /* ... SOME CODE MISSING HERE ... */
+  unsigned int *data;
+  size_t length;
+  size_t pops;
+  size_t pushes;
+  size_t max_length;
+  size_t capacity;
 };
 
 struct stack *stack_init(size_t capacity) {
-    /* ... SOME CODE MISSING HERE ... */
+  struct stack *s = malloc(sizeof(struct stack));
+  if (!s) return NULL;
+  unsigned int *data = malloc(capacity * sizeof(unsigned int));
+  if (!data) {
+    free(s);
+    return NULL;
+  }
+  s->length = 0;
+  s->max_length = 0;
+  s->data = data;
+  s->capacity = capacity;
+  return s;
 }
 
 void stack_cleanup(struct stack *s) {
-    /* ... SOME CODE MISSING HERE ... */
+  free(s->data);
+  free(s);
 }
 
 void stack_stats(const struct stack *s) {
-    /* ... SOME CODE MISSING HERE ... */
+  fprintf(stderr, "stats %lu %lu %lu", s->pushes, s->pops, s->max_length);
 }
 
 int stack_push(struct stack *s, int c) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (!s) return 1;
+  if (s->length >= s->capacity) {
+    return 1;
+  }
+  s->data[s->length++] = c;
+  if (s->length > s->max_length) {
+    s->max_length = s->length;
+  }
+  s->pushes++;
+  return 0;
 }
 
 int stack_pop(struct stack *s) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (!s) return -1;
+  if (!s->length) return -1;
+  s->pops++;
+  return s->data[--(s->length)];
 }
 
 int stack_peek(const struct stack *s) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (!s) return -1;
+  if (s->length == 0) {
+    return -1;
+  }
+  return s->data[s->length - 1];
 }
 
 int stack_empty(const struct stack *s) {
-    /* ... SOME CODE MISSING HERE ... */
+  if (!s) return -1;
+  return !s->length;
 }
 
 size_t stack_size(const struct stack *s) {
-    /* ... SOME CODE MISSING HERE ... */
+  return s->length;
 }
